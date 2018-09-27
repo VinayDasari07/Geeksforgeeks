fef64d117d1868a977cf81287786cba438c50aed
Vinay Dasari <vinay.dasari.07@gmail.com>
2018-09-27 23:06:56 +0530

    Update Check XOR.c

--- Arrays/Check XOR.c
+++ Arrays/Check XOR.c
@@ -1,8 +1,13 @@
-/* Given an array arr[] containing N positive integers. The task is to write a program to calculate the XOR of all of the elements of the array and then check if the right-most bit of the calculated XOR is set or not. 
+/* Given an array arr[] containing N positive integers. 
+The task is to write a program to calculate the XOR of all of the elements of the array and 
+then check if the right-most bit of the calculated XOR is set or not. 
 
-Input:  The first line of input is an integer T, denoting the number of test cases. For each test case, the first line of input is the integer N, the size of array arr[]. The second line of each test case contains N space separated integers denoting the elements of the array.
+Input:  The first line of input is an integer T, denoting the number of test cases. For each test case,
+the first line of input is the integer N, the size of array arr[]. The second line of each test case 
+contains N space separated integers denoting the elements of the array.
 
-Output: For each test case, print "YES" if the right-most bit in the XOR of all of the array elements is set otherwise print "NO" without quotes.
+Output: For each test case, print "YES" if the right-most bit in the XOR of all of the array elements is set 
+otherwise print "NO" without quotes.
  
 Constraints:
 1 <= T <= 100
